681ad0f10c88ffc604ca3f25daccb3c95b8fb574
asgharsir <asgharsir@gmail.com>
2020-09-16 18:05:44 +0500

    Added PHPUnit

--- PHP/tests/test_1.php
+++ PHP/tests/test_1.php
@@ -5,7 +5,7 @@ class test_1 extends TestCase
 {
 	public function testCalculationOfMean()
 	{
-		$numbers = [3, 7, 6, 1, 5];
-		$this->assertEquals(4.4, 4.45);
+        $this->assertEquals(4.4, 4.40);
+        $this->assertEquals(4.4, 4.44);
 	}
 }
