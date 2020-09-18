30decca22db8362bab0149043a9913e883e634f2
asgharsir <asgharsir@gmail.com>
2020-09-18 21:32:38 +0500

    added selenium

--- dj_tests/test_samples.py
+++ dj_tests/test_samples.py
@@ -1,4 +1,6 @@
 from rest_framework.test import APITestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class TestSamples(APITestCase):
 
@@ -6,3 +8,14 @@ class TestSamples(APITestCase):
         two = 2
         self.assertEqual(2, two)
         self.assertNotEqual(1, two)
+
+    def test_two(self):
+        driver = webdriver.FireFox()
+        driver.get("http://www.python.org")
+        assert "Python" in driver.title
+        elem = driver.find_element_by_name("q")
+        elem.clear()
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+        driver.close()
