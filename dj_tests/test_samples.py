1023dd1e2ea94ae00a6d31fccafc41f5e1069aa8
asgharsir <asgharsir@gmail.com>
2020-09-18 17:02:14 +0500

    added selenium test

--- dj_tests/test_samples.py
+++ dj_tests/test_samples.py
@@ -10,7 +10,7 @@ class TestSamples(APITestCase):
         self.assertNotEqual(1, two)
 
     def test_two(self):
-        driver = webdriver.Firefox()
+        driver = webdriver.Chrome()
         driver.get("http://www.python.org")
         assert "Python" in driver.title
         elem = driver.find_element_by_name("q")
